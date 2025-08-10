f70c1a6d90331416f25cbbcb72ace1fb8384d4b0
john <ypoh007@e.ntu.edu.sg>
2025-08-10 18:40:33 +0800

    Initial setup from https://start.spring.io/

---
+++ src/main/java/com/expensetracker/expensetracker_mvp/ExpensetrackerMvpApplication.java
@@ -0,0 +1,13 @@
+package com.expensetracker.expensetracker_mvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensetrackerMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensetrackerMvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expensetracker/expensetracker_mvp/ExpensetrackerMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expensetracker.expensetracker_mvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensetrackerMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
