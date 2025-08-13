c34af0a8367890a9f43a0354dd9d4e4e37688419
john <ypoh007@e.ntu.edu.sg>
2025-08-13 21:52:14 +0800

    removed unnecessary import

--- src/test/java/com/expensetracker/expensetracker_mvp/UserEntityMappingTest.java
+++ src/test/java/com/expensetracker/expensetracker_mvp/UserEntityMappingTest.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.Optional;
-import java.util.UUID;
 
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
