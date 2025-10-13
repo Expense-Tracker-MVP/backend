7aa291fce94a4a84fa548d6657129d3c1942b78b
john <ypoh007@e.ntu.edu.sg>
2025-10-13 22:49:01 +0800

    Enabled oauth in swaggerUI. Still WIP but usable by copying token from URL.

--- src/main/java/com/expensetracker/expensetracker_mvp/config/OpenApiConfig.java
+++ src/main/java/com/expensetracker/expensetracker_mvp/config/OpenApiConfig.java
@@ -1,27 +1,58 @@
 package com.expensetracker.expensetracker_mvp.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.OAuthFlow;
+import io.swagger.v3.oas.models.security.OAuthFlows;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
+    @Value("${server.port:8080}")
+    private int serverPort;
+
     @Bean
     public OpenAPI expenseTrackerOpenAPI() {
+        final String oauthSchemeName = "googleOAuth2";
+        final String bearerSchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Expense Tracker MVP API")
-                        .description("RESTful API for managing personal expenses, categories, and file attachments")
+                        .description("RESTful API for managing personal expenses, categories, and file attachments. " +
+                                "Click 'Authorize' below and login with Google to test the API.")
                         .version("v1.0")
                         .contact(new Contact()
                                 .name("Expense Tracker Team")
                                 .email("expensetrackermvp1234@gmail.com"))
                         .license(new License()
                                 .name("MIT License")
-                                .url("https://opensource.org/licenses/MIT")));
+                                .url("https://opensource.org/licenses/MIT")))
+                .addSecurityItem(new SecurityRequirement().addList(oauthSchemeName))
+                .addSecurityItem(new SecurityRequirement().addList(bearerSchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(oauthSchemeName,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.OAUTH2)
+                                        .description("Google OAuth2 Authentication")
+                                        .flows(new OAuthFlows()
+                                                .authorizationCode(new OAuthFlow()
+                                                        .authorizationUrl("http://localhost:" + serverPort
+                                                                + "/oauth2/authorization/google")
+                                                        .tokenUrl("https://oauth2.googleapis.com/token"))))
+                        .addSecuritySchemes(bearerSchemeName,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("JWT Bearer token from successful OAuth2 login")));
     }
 }
